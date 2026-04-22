f181e13701bc90620c704a98a6600fc71e07b74b
Cappuccino9 <yjkwon991223@gmail.com>
2026-04-23 04:23:01 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>{{ message }}</h1>
+    <div class="bg-red">asdssasd</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// 반응형 데이터 정의
+const message = ref('Hello, World!')
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+}
+h1 {
+  color: #42b883; /* Vue 공식 로고 색상 */
+  font-size: 3rem;
+}
+</style>
